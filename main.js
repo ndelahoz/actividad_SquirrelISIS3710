3e47f8d6d40ac862d9bf421169e6fc79a024161a
Nicolás De la Hoz <nicolasdelahoz2004@gmail.com>
2021-09-27 23:34:45 -0500

    Final Actividad

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const url = "https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e7321443
 
 fetch(url)
     .then(response => response.json())
-    .then(data => CreateTableFromJSON(data));
+    .then(data => {
+        CreateTableFromJSON(data);
+        CreateTableCorrelation(data);
+    });
 
 function CreateTableFromJSON(res) {
     // EXTRACT VALUE FOR HTML HEADER. 
-    var col = ['#'];
+    let col = ['#'];
     for (var i = 0; i < res.length; i++) {
         for (var key in res[i]) {
             if (col.indexOf(key) === -1) {
@@ -17,28 +20,28 @@ function CreateTableFromJSON(res) {
     }
 
     // CREATE DYNAMIC TABLE.
-    var table = document.createElement("table");
+    let table = document.createElement("table");
     table.className="table table-striped";
 
     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
 
-    var tr = table.insertRow(-1);   // TABLE ROW.
+    let tr = table.insertRow(-1);   // TABLE ROW.
                 
 
-    for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");      // TABLE HEADER.
+    for (let i = 0; i < col.length; i++) {
+        let th = document.createElement("th");      // TABLE HEADER.
         th.innerHTML = col[i];
         tr.appendChild(th);
     }
 
     // ADD JSON DATA TO THE TABLE AS ROWS.
-    for (var i = 0; i < res.length; i++) {
+    for (let i = 0; i < res.length; i++) {
 
          tr = table.insertRow(-1);
          
 
-         for (var j = 0; j < col.length; j++) {
-            var tabCell = tr.insertCell(-1);
+         for (let j = 0; j < col.length; j++) {
+            let tabCell = tr.insertCell(-1);
             if(j==0){
                 tabCell.innerHTML = i+1;
             }else{
@@ -58,45 +61,104 @@ function CreateTableFromJSON(res) {
     divContainer.appendChild(table);
 }
 
-function createTableCorrelation(res){
+//Funcion que crea la tabla de correlacion
+
+function CreateTableCorrelation(res){
 
     // Column names
-    var col = ['#', "Event", "Correlation"];
+    let col = ['#', "Event", "Correlation"];
+    let dict ={};
 
-    // CREATE DYNAMIC TABLE.
-    var table = document.createElement("table");
-    table.className="table table-striped";
-
-    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-    var tr = table.insertRow(-1);                   // TABLE ROW.
-
-    for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");      // TABLE HEADER.
-        th.innerHTML = col[i];
-        tr.appendChild(th);
-    }
-
-    // ADD JSON DATA TO THE TABLE AS ROWS.
+    //Creamos un diccionario con la lista de eventos
     for (var i = 0; i < res.length; i++) {
+        let events=res[i]["events"];
+        let tam =events.length;
+        for(let i =0; i<tam; i++){
 
-         tr = table.insertRow(-1);
-         tr.style.color="red";
-
-         for (var j = 0; j < col.length; j++) {
-            var tabCell = tr.insertCell(-1);
-            if(j==0){
-                tabCell.innerHTML = i+1;
-            }else{
-                tabCell.innerHTML = res[i][col[j]];
+           if(!dict.hasOwnProperty(events[i])){
+                dict[events[i]]={TN: 0, FN:0, TP:0, FP:0 };
             }
-            
-            
         }
     }
 
+    //Llenamos los datos del diccionario para caluclar el MCC
+    for (var i = 0; i < res.length; i++) {
+        let events=res[i]["events"];
+        let squirrel = res[i]["squirrel"];
+        
+        let tam= Object.keys(dict).length;
+        for(let event in dict){
+            if(events.includes(event)){
+                squirrel ? dict[event]["TP"]++: dict[event]["FN"]++;
+            }else{
+                squirrel ? dict[event]["FP"]++: dict[event]["TN"]++;
+            }
+        }
+    }
+    
+
+    //MCC for each event
+    let MCC={};
+    for(let event in dict){
+        let MCCevent= CalculateMCC(dict[event]);
+        MCC[MCCevent]=event;
+    }
+    let datosTabla=Object.keys(MCC).sort((a,b)=>b-a);
+
+    //Creamos la tabla ahora en orden
+    // CREATE DYNAMIC TABLE.
+    let table = document.createElement("table");
+    table.className="table table-striped";
+    
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+    
+    let tr = table.insertRow(-1);   // TABLE ROW.
+                    
+    
+    for (let i = 0; i < col.length; i++) {
+        let th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+    
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (let i = 0; i < datosTabla.length; i++) {
+    
+            tr = table.insertRow(-1);
+    
+            for (let j = 0; j < col.length; j++) {
+            let tabCell = tr.insertCell(-1);
+            
+            if(j==0){
+                tabCell.innerHTML = i+1;
+            }else if(j==1){
+                tabCell.innerHTML = MCC[datosTabla[i]];
+                tabCell.className="col-8";
+            }else{
+                tabCell.innerHTML =datosTabla[i];
+            }
+    
+                
+        }
+     }
+    
     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-    var divContainer = document.getElementById("showData");
+    var divContainer = document.getElementById("showDataCorrelation");
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
+
+
+
+}
+
+function CalculateMCC(data){
+
+    let TP=data["TP"];
+    let TN=data["TN"];
+    let FP=data["FP"];
+    let FN=data["FN"];
+
+    let num= TP*TN-FP*FN;
+    let denom= Math.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN));
+    return num/denom;
 }
